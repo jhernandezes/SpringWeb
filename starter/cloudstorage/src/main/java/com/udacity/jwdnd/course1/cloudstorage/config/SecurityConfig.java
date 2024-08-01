059f96c95ef9ee2196cb6bfd70d1f14f1ace3db3
Javier Hernández <jhernandez@vitio.es>
2024-08-01 16:34:38 +0200

    Redirect to login page when logout is done

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -7,15 +7,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.authorizeRequests()
                 .antMatchers("/signup", "/css/**", "/js/**").permitAll()
                 .anyRequest().authenticated();
 
         http.formLogin()
                 .loginPage("/login")
-                .permitAll();
-
-        http.formLogin()
+                .permitAll()
                 .defaultSuccessUrl("/home", true);
+        
+        http.logout().logoutSuccessUrl("/login");
+
     }
 }
