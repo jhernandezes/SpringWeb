e55471d5ac1fb654aaf3becff9dfee4ce05a0ee5
Javier Hernández <jhernandez@vitio.es>
2024-08-16 23:51:59 +0200

    [Note] feedback message for CRUD

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -45,13 +46,14 @@ public class NoteController {
     }
 
     @GetMapping("/delete/{noteTitle}")
-    public String deleteNote(@PathVariable("noteTitle") String noteTitle, Model model, Authentication authentication) {
+    public String deleteNote(@PathVariable("noteTitle") String noteTitle, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
         notesService.deleteNote(noteTitle);
+        redirectAttributes.addFlashAttribute("result", "Note deleted successfully.");
         return "redirect:/home";
     }
 
     @PostMapping
-    public String handleNoteUpload(Note noteUpload, Authentication authentication, Model model) {
+    public String handleNoteUpload(Note noteUpload, Authentication authentication, Model model, RedirectAttributes redirectAttributes) {
 
         String username = authentication.getName();
         Integer userId = userMapper.getUser(username).getUserId();
@@ -63,9 +65,12 @@ public class NoteController {
         if (newNote) {
             notesService.addNote(noteUpload);
             model.addAttribute("notes", noteUpload);
-        }else
+            redirectAttributes.addFlashAttribute("result", "Note created successfully.");
+        } else {
             notesService.updateNote(noteUpload);
+            redirectAttributes.addFlashAttribute("result", "Note updated successfully.");
 
+        }
         return "redirect:/home";
     }
 }
